0e1067619638bce2af70f642c906e1bdefe4de09
Tobias Meier <Tobias.Meier@bridging-it.de>
2017-05-18 12:05:47 +0200

    Header bei Upload mitschicken

--- PhotoAward/PhotoAward.AngularClient/src/app/Shared/uploadService.ts
+++ PhotoAward/PhotoAward.AngularClient/src/app/Shared/uploadService.ts
@@ -1,53 +1,54 @@
-import {Observable} from 'rxjs/Rx';
+import { UserService } from './user.service';
+import { Observable } from 'rxjs/Rx';
 import { PhotoManagementClient, API_BASE_URL } from './Controllers.generated';
-import { Component, Injectable, Inject, Optional, OpaqueToken} from '@angular/core';
-
-
+import { Component, Injectable, Inject, Optional, OpaqueToken } from '@angular/core';
 @Injectable()
 export class UploadService {
   progress$: any;
   progress: any;
   progressObserver: any;
-  private _baseUrl:string;
+  private _baseUrl: string;
 
-  constructor( @Optional() @Inject(API_BASE_URL) baseUrl?: string ) {
+  constructor(private userService: UserService, @Optional() @Inject(API_BASE_URL) baseUrl?: string) {
     this._baseUrl = baseUrl ? baseUrl : "";
-   }
+  }
 
- uploadFile(email:string, title:string, filename:string, params: string[], files: File[])  {
+  uploadFile(email: string, title: string, filename: string, params: string[], files: File[]) {
+    return new Promise<string>((resolve, reject) => {
 
-   return new Promise<string>((resolve, reject) => {
+      const url = this._baseUrl + '/api/photo/uploadPhoto';
+      const formData: FormData = new FormData(),
+        xhr: XMLHttpRequest = new XMLHttpRequest();
 
-   const url = this._baseUrl + '/api/photo/uploadPhoto';
-   const formData: FormData = new FormData(),
-            xhr: XMLHttpRequest = new XMLHttpRequest();
+      for (let i = 0; i < files.length; i++) {
+        formData.append("uploads[]", files[i], files[i].name);
+      }
 
-        for (let i = 0; i < files.length; i++) {
-            formData.append("uploads[]", files[i], files[i].name);
-        }
-
-        xhr.onreadystatechange = () => {
-          if (xhr.readyState ===1) {
-            xhr.setRequestHeader("email",email);
-            xhr.setRequestHeader("title", title);
-            xhr.setRequestHeader("filename", filename);
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === 1) {
+          if (this.userService.token)    {
+            xhr.setRequestHeader('Authorization','Bearer ' +this.userService.token.access_token);
           }
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(xhr.responseText); //OK
-                } else {
-                    reject(xhr.statusText); //Error
-                }
-            }
-        };
+          xhr.setRequestHeader("email", email);
+          xhr.setRequestHeader("title", title);
+          xhr.setRequestHeader("filename", filename);
+        }
+        if (xhr.readyState === 4) {
+          if (xhr.status === 200) {
+            resolve(xhr.responseText); //OK
+          } else {
+            reject(xhr.statusText); //Error
+          }
+        }
+      };
 
 
-        xhr.upload.onprogress = (event) => {
-        };
+      xhr.upload.onprogress = (event) => {
+      };
 
-        xhr.open('POST', url, true);
-        const serverFileName = xhr.send(formData);
-        return serverFileName;
-   });  //Promise
+      xhr.open('POST', url, true);
+      const serverFileName = xhr.send(formData);
+      return serverFileName;
+    });  //Promise
   }
 }
